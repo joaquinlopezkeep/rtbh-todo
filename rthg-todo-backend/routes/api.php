bf5b9eff9120878b84f7d3702f594cb1acf2064a
joaquinlopezkeep <joaquinlopezkeep@gmail.com>
2023-09-23 21:27:43 +0100

    testing laravel pint code quality tool set up

--- rthg-todo-backend/routes/api.php
+++ rthg-todo-backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\TaskListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,18 +16,17 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-Route::prefix('v1/tasks')->group(function (){
+Route::prefix('v1/tasks')->group(function () {
     Route::get('/complete', [TaskController::class, 'complete']);
     Route::get('/incomplete', [TaskController::class,'incomplete']);
     //The Routes above have to come first.
     Route::apiResource('/tasks', TaskController::class);
 });
 
-Route::prefix('v1/task-lists')->group(function (){
+Route::prefix('v1/task-lists')->group(function () {
     Route::apiResource('/tasks', TaskListController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
